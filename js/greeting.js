c816db3e2c05f8deb3ecdd8b5cd2181c42375ac1
조현일 <himzei@gmail.com>
2022-08-16 08:50:47 +0900

    css

--- js/greeting.js
+++ js/greeting.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `${username}님 오늘도 즐코딩 하세요!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
